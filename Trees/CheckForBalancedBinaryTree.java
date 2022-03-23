2629193b9de459e704df59886cf8c0a5b089f8c2
unknown <akshaymadiwalar@gmail.com>
2022-03-23 02:27:00 -0700

    balanced biary tree

---
+++ Trees/CheckForBalancedBinaryTree.java
@@ -0,0 +1,39 @@
+package Trees;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    
+    boolean ans = true;
+    
+    public int height(TreeNode node){
+        if(node == null){
+            return 0;
+        }
+        int lheight = height(node.left);
+        int rheight = height(node.right);
+        
+        if(Math.abs(lheight - rheight) > 1){
+            ans = false;
+        }
+        
+        if(lheight> rheight){
+            return lheight + 1;
+        }
+        return rheight + 1;
+    }
+    public boolean isBalanced(TreeNode root) {
+        int h = height(root);
+        return ans;
+    }
+}
