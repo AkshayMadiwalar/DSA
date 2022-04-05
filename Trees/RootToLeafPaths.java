244b0b03b84c8e74b072af2cbb2f0d3f376cc6b4
unknown <akshaymadiwalar@gmail.com>
2022-04-04 19:45:15 -0700

    node to leaf paths

---
+++ Trees/RootToLeafPaths.java
@@ -0,0 +1,38 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    List<String> ans = new ArrayList<>();
+    
+    public void io(TreeNode root){
+        if(root ==null){
+            return;
+        }
+        
+        list.add(root.val);
+        io(root.left);
+        io(root.right);
+        
+        if(root.left == null && root.right ==null){
+            //reached leaf
+            String path = "";
+            for(int i=0;i<list.size();i++){
+                if(i==list.size()-1){
+                    path = path + list.get(i);
+                }else{
+                    path = path + list.get(i) + "->";
+                }
+            }
+            ans.add(path);
+        }
+        list.remove(list.size()-1);
+        
+    }
+    public List<String> binaryTreePaths(TreeNode root) {
+        io(root);
+        return ans;
+    }
+}
